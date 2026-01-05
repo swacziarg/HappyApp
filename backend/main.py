93052cac34d99f5a49dd9fac93b9c454348029e7
Simon Wacziarg <swacziarg@gmail.com>
2026-01-04 19:12:47 -0800

    fixed cors again

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://swaczlarg.github.io", 
+        "https://swacziarg.github.io", 
     ],
     allow_credentials=True,
     allow_methods=["*"],
