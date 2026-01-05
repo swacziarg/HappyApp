4369cdb9cc155c164104c9e3fb36e9e484f620ca
Simon Wacziarg <swacziarg@gmail.com>
2026-01-04 19:06:09 -0800

    fixed cors access bug

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://your-github-username.github.io", 
+        "https://swaczlarg.github.io", 
     ],
     allow_credentials=True,
     allow_methods=["*"],
